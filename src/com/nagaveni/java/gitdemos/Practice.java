bbbfa57b4ab913a7dd6efb46f2928be94b39df2b
nagaveni4723 <nagaveni.gujipineni@gmail.com>
2022-11-13 13:08:15 +0530

    added new file job and added some to practice

--- src/com/nagaveni/java/gitdemos/Practice.java
+++ src/com/nagaveni/java/gitdemos/Practice.java
@@ -1,5 +1,12 @@
 package com.nagaveni.java.gitdemos;
 
-public class Practice {
+public class Practice 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello1");
+		System.out.println(ello);
+		
+	}
 
 }
---
+++ src/com/nagaveni/java/gitdemos/job.java
@@ -0,0 +1,13 @@
+package com.nagaveni.java.gitdemos;
+
+public class job 
+{public static void main(String[] args) 
+{
+	System.out.println("Hai1");
+	System.out.println(elo);
+	
+}
+
+}
+
+
